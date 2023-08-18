31b3935c8f3aa92907dd3ca5f0534f22835d62a8
Kathir2321 <142398049+Kathir2321@users.noreply.github.com>
2023-08-18 18:16:12 +0530

    Add files via upload

---
+++ function.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+ 
+ function proGeek($num1, $num2, $num3)
+ {
+     $product = $num1 * $num2 * $num3;
+     echo "The product is $product";
+ }
+ proGeek(2, 3, 5);
+  
+ ?>
+</body>
+</html>
